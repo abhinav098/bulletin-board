6efe27a6d4ba32f4c3c6ebd3ffe4111d557ddf4e
Abhinav Garg <abhinav.g@kiprosh.com>
2019-01-17 23:54:06 +0530

    finish

---
+++ src/Board.js
@@ -0,0 +1,64 @@
+import React, { Component }  from "react";
+import Note from './Note';
+import { FaPlus } from "react-icons/fa";
+
+class Board extends Component {
+  state = {
+    notes: []
+  }
+
+  update = (newText, i) => {
+    console.log('updating item', i, newText);
+    this.setState(prevState => ({
+      notes: prevState.notes.map(
+        note => (note.id !== i) ? note : {...note, note: newText}
+      )
+    }))
+  }
+
+  remove = (id) => {
+    console.log('removing at', id)
+    this.setState(prevState => ({
+      notes: prevState.notes.filter(note => note.id !== id)
+    }))
+  }
+
+  eachNote = (note, i) => {
+    return (
+      <Note
+        key={i}
+        index={i}
+        onChange={this.update}
+        onRemove={this.remove}>
+        {note.note}
+      </Note>
+    )
+  }
+
+  addNote = (text) => {
+    this.setState( prevState => ({
+      notes: [...prevState.notes,
+        {
+          id: this.nextId(),
+          note: text
+        }
+      ]
+    }))
+  }
+
+  nextId = () => {
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+  }
+
+  render(){
+    return(
+      <div className="board">
+        <button onClick={this.addNote.bind(null, "New Note")} id="add"><FaPlus /></button>
+        { this.state.notes.map(this.eachNote) }
+      </div>
+    )
+  }
+}
+
+export default Board;
--- src/Note.js
+++ src/Note.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react'
+import { FaPencilAlt } from "react-icons/fa";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaSave } from "react-icons/fa";
+import { throws } from 'assert';
+
 
 class Note extends Component{
-  render(){
+  state = {
+    editing: false
+  }
+
+  // Other way of binding the methods and defining state
+
+  // constructor(props){
+  //  super();
+  //  this.state = {
+  //    editing: false
+  //  }
+  //  this.edit = this.edit.bind(this);
+  //  this.remove = this.remove.bind(this);
+  //  this.renderDisplay = this.renderDisplay.bind(this);
+  //  this.renderForm = this.renderForm.bind(this);
+  // }
+
+  edit = () => {
+    this.setState({ editing: true });
+  }
+
+  renderForm = () => {
     return <div className="note">
-        <p>This is note</p>
-        <span>
-          <button>Edit</button>
-          <button>Remove</button>
-        </span>
+      <form onSubmit={this.save}>
+          <textarea ref={input => this._newText = input}/>
+          <button id="save">
+            <FaSave />
+          </button>
+        </form>
       </div>;
+  }
+
+  save = (e) => {
+    debugger
+    e.preventDefault();
+    this.props.onChange(this._newText.value, this.props.index);
+    this.setState({
+      editing: false
+    })
+  }
+
+  remove = () => {
+    this.props.onRemove(this.props.index);
+  }
+
+  renderDisplay = () => {
+    return (<div className="note">
+        <p>{this.props.children}</p>
+        <span>
+          <button id="edit" onClick={ this.edit }>
+            <FaPencilAlt />
+          </button>
+        <button id="remove" onClick={ this.remove }>
+            <FaTrashAlt />
+          </button>
+        </span>
+    </div>);
+  }
+
+  render(){
+    return this.state.editing ? this.renderForm() : this.renderDisplay();
   }
 }
 
